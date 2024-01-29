c004efb7288828ffb824871e24b83ce35bcc3770
Nardos <contactnardos@gmail.com>
2024-01-29 01:55:34 +0000

    2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,31 +1,35 @@
 ###
 #
-#  Sort integer 
+#  Sort integer
 #
 ###
 
-result = []
-ARGV.each do |arg|
-    # if not integer
-    next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
+result = []
+
+ARGV.each do |arg|
+    # Check if it's an integer
+    next unless arg =~ /^-?\d+$/
+
+    # Convert to integer
     i_arg = arg.to_i
-    
-    # insert at the right position
+
+    # Find the correct position to insert
     is_inserted = false
     i = 0
     l = result.size
+
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
+        if result[i] > i_arg
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
+        i += 1
     end
-    result << i_arg if !is_inserted
+
+    result << i_arg unless is_inserted
 end
 
-puts result
+puts result.reverse
+
